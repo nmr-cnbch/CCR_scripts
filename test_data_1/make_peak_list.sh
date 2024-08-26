776eadcf0772787c0a1181806bd4ce1bb90011fe
PBMarzec <100852078+PBMarzec@users.noreply.github.com>
2024-08-26 19:22:14 +0200

    Update make_peak_list.sh

--- test_data_1/make_peak_list.sh
+++ test_data_1/make_peak_list.sh
@@ -1,2 +1,2 @@
-/nopt/anaconda/bin/python ../read_ucsf_objectowo.py 2D_14_CCR_5_a.ucsf ../N_HSQC_Ubiq_2mM_2024_centrowane_na_CCR_5.list -o 2D_14_CCR_5_a.list -sn 5.0 
-/nopt/anaconda/bin/python ../read_ucsf_objectowo.py 2D_15_CCR_5_x.ucsf ./2D_14_CCR_5_a_list/2D_14_CCR_5_a_new_ppm.list -o 2D_15_CCR_5_x.list -nrm 
+python3 ../read_ucsf.py 2D_CCR_5_a.ucsf ./CCR_5.list -o 2D_CCR_5_a.list -sn 5.0 
+python3 ../read_ucsf.py 2D_CCR_5_x.ucsf ./2D_CCR_5_a_list/2D_CCR_5_a_new_ppm.list -o 2D_CCR_5_x.list -nrm 
