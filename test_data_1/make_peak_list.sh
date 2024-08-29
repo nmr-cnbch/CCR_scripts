753e76d47921a28dcb18f8ab842a88fd2ff2b70e
anna-zk <92855065+anna-zk@users.noreply.github.com>
2024-08-29 13:50:11 +0200

    Delete test_data_1/make_peak_list.sh

--- test_data_1/make_peak_list.sh
+++
@@ -1,2 +0,0 @@
-python3 ../read_ucsf.py 2D_CCR_5_a.ucsf ./CCR_5.list -o 2D_CCR_5_a.list -sn 5.0 
-python3 ../read_ucsf.py 2D_CCR_5_x.ucsf ./2D_CCR_5_a_list/2D_CCR_5_a_new_ppm.list -o 2D_CCR_5_x.list -nrm 
